af19aaded94bedae8dffb7a2531bc2a7315d1a18
qtaro1001 <yosoyokuno+cloud9@gmail.com>
2015-10-26 09:55:10 +0000

    initial commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :followings, :followers]
+                                        
   def show
    @user = User.find(params[:id])
    @microposts = @user.microposts
@@ -13,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"      
-      redirect_to @user # ここを修正
+      redirect_to @user 
     else
       render 'new'
     end
@@ -27,12 +29,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
-      redirect_to @user
+      redirect_to @user.
     else
       render 'edit'
     end
   end
   
+  def followings
+    @user  = User.find(params[:id])
+    @users = @user.following_relationships
+    render 'followings'
+  end
+
+  def followers
+    @user  = User.find(params[:id])
+    @users = @user.follower_relationships
+    render 'followers'
+  end
+
+    
   private
 
   def user_params
